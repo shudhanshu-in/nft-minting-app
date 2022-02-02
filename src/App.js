e1d9ee7c4c3dbd6c0070e305e6ec046d10233574
Shudhanshu Upadhyay <88653260+shudhanshu-in@users.noreply.github.com>
2022-02-02 23:55:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -270,6 +270,11 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
+              <s.TextTitle
+                  style={{ textAlign: "center", color: "var(--accent-text)" , fontSize: 50, fontWeight: "bold",}}
+                >
+                  Public Mint is LIVE
+                </s.TextTitle>
               <CountUp  start={0} end={data.totalSupply} duration={1} /> / {CONFIG.MAX_SUPPLY}
             </s.TextTitle>
             
