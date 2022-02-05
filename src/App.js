3240f059ef2a4432056724ded759780bd4be651a
Shudhanshu Upadhyay <88653260+shudhanshu-in@users.noreply.github.com>
2022-02-05 10:34:43 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -416,13 +416,7 @@ function App() {
             <s.SpacerMedium />
           </s.Container>
           <s.SpacerLarge />
-          <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg
-              alt={"example"}
-              src={"/config/images/example.gif"}
-              style={{ transform: "scaleX(-1)" }}
-            />
-          </s.Container>
+          
         </ResponsiveWrapper>
         <s.SpacerMedium />
         <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
